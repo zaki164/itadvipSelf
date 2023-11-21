cd101d73be8b684805a1855c563c48d4d6fbeac4
zaki164 <zakimohsen14@gmail.com>
2023-11-21 06:52:37 +0200

    assets , fonts path to public

--- src/Constants/images.js
+++ src/Constants/images.js
@@ -1,60 +1,14 @@
-import facebook from "../assets/facebook.png";
-import instagram from "../assets/instagram.png";
-import pinterest from "../assets/pinterest.png";
-import snapchat from "../assets/PRIMARY_-_GHOST.png";
-import tiktok from "../assets/tik-tok.png";
-import twitter from "../assets/twitter-alt.png";
-import logo from "../assets/logo.png";
-import diamond from "../assets/diamond.png";
-import gauge from "../assets/gauge-circle-bolt.png";
-import shield from "../assets/shield-check.svg";
-import weInfo from "../assets/weInfo.png";
-import ceo from "../assets/ceo.png";
-import mandobak from "../assets/mandobak.png";
-import mandobak1 from "../assets/mandobak1.png";
-import mandobak2 from "../assets/mandobak2.png";
-import mandobak3 from "../assets/mandobak3.png";
-import mandobak4 from "../assets/mandobak4.png";
-import mandobak5 from "../assets/mandobak5.png";
-import PlayStore from "../assets/PlayStore.svg";
-import AppStore from "../assets/AppStore.svg";
-import illustration1 from "../assets/illustration1.png";
-import illustration2 from "../assets/illustration2.png";
-import person from "../assets/person.png";
-import amr from "../assets/amr.jpeg";
-import avatarThinking from "../assets/avatar-thinking.svg";
-import contactImg from "../assets/contactImg.png";
-import mandobakImage from "../assets/mandobakImage.png";
-import huawei from "../assets/huawei.jpg";
+import logo from "/assets/logo.png";
+import avatarThinking from "/assets/avatar-thinking.svg";
+import PlayStore from "/assets/PlayStore.svg";
+import AppStore from "/assets/AppStore.svg";
+import huawei from "/assets/huawei.jpg";
 
 const images = {
-  facebook,
-  instagram,
-  pinterest,
-  snapchat,
-  tiktok,
-  twitter,
   logo,
-  diamond,
-  gauge,
-  shield,
-  weInfo,
-  ceo,
-  mandobak,
-  mandobak1,
-  mandobak2,
-  mandobak3,
-  mandobak4,
-  mandobak5,
+  avatarThinking,
   PlayStore,
   AppStore,
-  illustration1,
-  illustration2,
-  person,
-  amr,
-  avatarThinking,
-  contactImg,
-  mandobakImage,
   huawei,
 };
 
